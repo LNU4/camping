29a4de5cea917a226ba7a04125fd8fa678b6a407
LNU4 <112617909+LNU4@users.noreply.github.com>
2023-04-24 16:23:31 +0200

    latest update,
    further adjustments to the following:
    
    * Boxest has been replaced, and adjusted according to the hi fi prototype

--- script/index-script.js
+++ script/index-script.js
@@ -1,30 +1,47 @@
-
 function init() {
-
     addClickEventListeners();
-
-}
-window.addEventListener("load", init);
-
-
-
-function addClickEventListeners() {
-    const campingTypes = document.querySelectorAll(".camping-type-trailer, .camping-type-housecar, .camping-type-tent, .camping-type-stuga");
-    
-    let tralierCamping = document.getElementsByClassName("camping-type-trailer"); 
-    tralierCamping.addClickEventListener("click", hideEelem()); 
-
+  }
+  
+  window.addEventListener("load", init);
+  
+    function addClickEventListeners() {
+    const campingTypes = document.querySelectorAll(".camping-type-trailer, .camping-type-housecar, .camping-type-tent, .camping-type-stuga, .no-perferance, .oland, .smoland, .all-landscape, .yes, .no");
+  
+    let trailerCamping = document.getElementsByClassName("camping-type-trailer");
+    trailerCamping[0].addEventListener("click", hideElem);
+  
     for (let i = 0; i < campingTypes.length; i++) {
-    campingTypes[i].addEventListener("click", function () {
+      campingTypes[i].addEventListener("click", hideElem);
+      campingTypes[i].addEventListener("mouseover", function () {
+        this.style.cursor = "pointer";
         
-    }, false); 
-    campingTypes[i].addEventListener("mouseover", function () {
-        this.style.cursor = "pointer"
-    }, false); 
+      }, false);
     }
+    wedigtsHoverEffect();
+    
+  }
+  
+function hideElem() {
+    alert("Hello! I am an alert box!!");
 
 }
 
-hideEelem() {
-    let test 
+function showFilterElem() {
+
+}
+
+function wedigtsHoverEffect () {
+    let wedigts = document.querySelectorAll(".logoImg");
+    for (let i = 0; i < wedigts.length; i++) {
+        console.log(wedigts[i])
+
+      wedigts[i].addEventListener("mouseover", function () {
+        wedigts[i].style.border = "1px solid black";
+      }, false);
+
+      wedigts[i].addEventListener("mouseleave", function () {
+        wedigts[i].style.border = "";
+      }, false);
+    }
+    
 }
