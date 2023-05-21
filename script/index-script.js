0f2d2880a0cfcf2fddaea1c0a4960bbe322c2ca8
LNU4 <nasser.alobaedi@gmail.com>
2023-05-21 03:17:41 +0200

    fixed the issue with read more, new function, less code

--- script/index-script.js
+++ script/index-script.js
@@ -9,13 +9,13 @@ function init() {
   campingTypes = document.querySelectorAll(" .oland, .smoland, .all-landscape");
   addClickEventListeners();
   wedigtsHoverEffect();
-  
+
 }
 
 window.addEventListener("load", init);
 
 function addClickEventListeners() {
-  
+
   document.getElementById("filterDropMenu").addEventListener("change", SelectedOption);
 
   for (let i = 0; i < campingTypes.length; i++) {
@@ -49,7 +49,7 @@ function wedigtsHoverEffect() {
 function SelectedOption() {
   let filterOption = document.getElementById("filterDropMenu");
   let selectedFliterOption = filterOption.value;
-  let btnSelector = this.classList;  
+  let btnSelector = this.classList;
   filterOption.selectedIndex = 0;
   showFilterElem(selectedFliterOption, btnSelector);
 
@@ -57,32 +57,32 @@ function SelectedOption() {
 
 function showFilterElem(selectedFliterOption, btnSelector) {
   let hiddenElems = document.getElementsByClassName("body-box-2");
-  
+
   resultatElem.innerHTML = "";
-  
+
   for (let i = 0; i < hiddenElems.length; i++) {
     hiddenElems[i].style.display = "grid";
   }
-  
-  if (btnSelector.contains("smoland")){
+
+  if (btnSelector.contains("smoland")) {
     url = "https://smapi.lnu.se/api/?api_key=" + myApiKey + "&debug=true&controller=establishment&method=getall&provinces=småland&descriptions=camping";
-  
-  } 
+
+  }
   else if (btnSelector.contains("oland")) {
     url = "https://smapi.lnu.se/api/?api_key=" + myApiKey + "&debug=true&controller=establishment&method=getall&provinces=öland&descriptions=camping";
 
   }
-   else if (btnSelector.contains ("all-landscape")) {
+  else if (btnSelector.contains("all-landscape")) {
     url = "https://smapi.lnu.se/api/?api_key=" + myApiKey + "&debug=true&controller=establishment&method=getall&descriptions=camping";
   }
 
   if (selectedFliterOption === "Pris") {
-     url += "&sort_in=ASC&order_by=price_range";
+    url += "&sort_in=ASC&order_by=price_range";
     console.log(url)
-  
-  } 
+
+  }
   else if (selectedFliterOption === "Omdöme") {
-  url += "&sort_in=DESC&order_by=rating";
+    url += "&sort_in=DESC&order_by=rating";
   }
 
   fetch(url)
@@ -95,81 +95,45 @@ function showFilterElem(selectedFliterOption, btnSelector) {
     })
     .then(data => {
       let detailElem = data.payload;
-  
+
       for (let i = 0; i < detailElem.length; i++) {
+
         let container = document.createElement("div");
         container.classList.add("campingRes");
-    
+
         container.setAttribute("cid", detailElem[i].id);
-    
+
         let pElement0 = document.createElement("p");
- 
+
         let pElement = document.createElement("h3");
         pElement.innerText = detailElem[i].name;
-        pElement.innerText += " - " + detailElem[i].province; 
+        pElement.innerText += " - " + detailElem[i].province;
         pElement.classList.add("nameElement");
-    
+
         let pElement2 = document.createElement("div");
-        let rating = parseFloat(detailElem[i].rating); 
+        let rating = parseFloat(detailElem[i].rating);
         pElement2.innerHTML = convertRating(rating);
         pElement2.classList.add("ratingElement");
-    
+
         let pElement3 = document.createElement("p");
-        pElement3.innerText = "Prisnivå: " + detailElem[i].price_range+ " SEK";
+        pElement3.innerText = "Prisnivå: " + detailElem[i].price_range + " SEK";
         pElement3.classList.add("priceRangeElement");
-    
+
         let pElement4 = document.createElement("p");
-        pElement4.innerText = detailElem[i].text;
+        let lessDetailString = detailElem[i].text.slice(0,100);
+        let moreDetailString = detailElem[i].text.slice(100);
+
+        let readMorebutton = document.createElement("button");
+        readMorebutton.innerText = " Läs mer";
+        createDetailButtons(pElement4, readMorebutton, lessDetailString, moreDetailString);
+        
+        pElement4.innerText = lessDetailString + "...";
+        pElement4.append(readMorebutton);
         pElement4.classList.add("textElement");
-        
-        if (pElement4.innerText === "") {
-          pElement4.innerText = "Det finns inga information om platsen"
-        }
 
-        let targetElements = document.getElementsByClassName("textElement"); 
-
-for (let j = 0; j < targetElements.length; j++) {
-  let targetElement = targetElements[j];
-  let text = targetElement.innerText.trim();
-  let wordLimit = 200;
-
-  if (text.length > wordLimit) {
-    let reducedText = text.slice(0, wordLimit);
-    let remainingText = text.slice(wordLimit);
-
-    let showMoreButton = document.createElement("span");
-    showMoreButton.classList.add("showMoreButton");
-
-    targetElement.innerHTML = reducedText;
-    showMoreButton.innerText = " ...läs mer";
-
-    let parentContainer = document.createElement("div");
-    parentContainer.appendChild(showMoreButton);
-    targetElement.appendChild(parentContainer);
-
-    showMoreButton.addEventListener("click", function () {
-      targetElement.innerHTML = reducedText + remainingText;
-
-      let showLessButton = document.createElement("span");
-      showLessButton.classList.add("showLessButton");
-      showLessButton.innerText = "läs mindre";
-      showLessButton.addEventListener("click", function () {
-        targetElement.innerHTML = reducedText;
-        targetElement.appendChild(showMoreButton);
-        parentContainer.removeChild(showLessButton);
-      });
-
-      targetElement.appendChild(showLessButton);
-      parentContainer.removeChild(showMoreButton);
-    });
-  }
-  
-}
-        
-        
-        }
         
 
+        let targetElements = document.getElementsByClassName("textElement");
         let logo = document.createElement("img");
         logo.classList.add("logoElement");
 
@@ -177,12 +141,19 @@ for (let j = 0; j < targetElements.length; j++) {
         linkElement.innerText = " Se mer "
         linkElement.classList.add("linkButton");
         linkElement.addEventListener("click", () => linkToFilterPage(detailElem[i].id));
-    
-        
-        container.append(pElement0, pElement, pElement2, pElement3, pElement4, logo, linkElement);
+
+
+        container.append(
+          pElement0,
+          pElement,
+          pElement2,
+          pElement3,
+          pElement4,
+          logo,
+          linkElement);
         resultatElem.append(container);
         imgUrlCall();
-        
+
       }
 
     })
@@ -190,23 +161,37 @@ for (let j = 0; j < targetElements.length; j++) {
       console.error("Det finns probleme med kommunikationen", error);
     });
 
-    
-    
 }
 
+function createDetailButtons(p4Element, readMorebutton, lessDetailString, moreDetailString) {
+  let readLessButton = document.createElement("button");
+  readLessButton.innerText = " Läs mindre";
+  readLessButton.addEventListener("click", () => {
+    p4Element.innerText = lessDetailString + "..."
+    p4Element.append(readMorebutton)
+  })
+
+  readMorebutton.addEventListener("click", () => {
+    p4Element.innerText = lessDetailString + moreDetailString + " ";
+    p4Element.append(readLessButton);
+  });
+
+}
+
+
 function convertRating(rating) {
-  let starImg = "../SVGassets/fullStar.svg"; 
-  let halfStarImg = "../SVGassets/halfStar.svg"; 
-  let emptyStarImg = "../SVGassets/noStar.svg"; 
+  let starImg = "../SVGassets/fullStar.svg";
+  let halfStarImg = "../SVGassets/halfStar.svg";
+  let emptyStarImg = "../SVGassets/noStar.svg";
   let ratingImgs = [];
 
   for (let i = 1; i <= 5; i++) {
     if (rating >= i) {
-      ratingImgs.push(starImg); 
+      ratingImgs.push(starImg);
     } else if (rating >= i - 0.5) {
-      ratingImgs.push(halfStarImg); 
+      ratingImgs.push(halfStarImg);
     } else {
-      ratingImgs.push(emptyStarImg); 
+      ratingImgs.push(emptyStarImg);
     }
   }
 
@@ -230,7 +215,7 @@ function imgUrlCall() {
       }
     })
     .then(imgData => {
-    
+
       let res = document.getElementsByClassName("campingRes");
       for (let i = 0; i < res.length; i++) {
         let elem = res[i];
@@ -255,6 +240,6 @@ function findIn(stack, key, value) {
 }
 
 function linkToFilterPage(id) {
-  let url = "guidePage.html?id=" +id;
-  window.location.href = url;  
+  let url = "guidePage.html?id=" + id;
+  window.location.href = url;
 }
